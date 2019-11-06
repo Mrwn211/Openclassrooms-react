12407a69f3bd5ca98e4188fc4f5570a606711c50
Mrwn211 <merwanguerrib@gmail.com>
2019-11-06 15:52:29 +0100

    First iterations of the course

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+import GuessCount from "./GuessCount";
+import Card from "./Card";
+
+
+class App extends Component {
+  handleCardClick(card) {
+    console.log(card, "clicked")
+  }
+
+  render() {
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
