83ab35fc7b34147eeeac6af8f2a11991f5a95165
Merwan Guerrib <merwanguerrib@gmail.com>
2019-11-16 21:18:41 +0100

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-import React, { Component } from 'react'
-import shuffle from 'lodash.shuffle'
+import React, { Component } from "react";
+import shuffle from "lodash.shuffle";
 
-import './App.css'
+import "./App.css";
 
-import Card from './Card'
-import GuessCount from './GuessCount'
-import HallOfFame, { FAKE_HOF } from './HallOfFame'
+import Card from "./Card";
+import GuessCount from "./GuessCount";
+import HallOfFame, { FAKE_HOF } from "./HallOfFame";
 
-const SIDE = 6
-const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const SIDE = 6;
+const SYMBOLS = "😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿";
 
 class App extends Component {
-  cards = this.generateCards()
+  cards = this.generateCards();
 
   generateCards() {
-    const result = []
-    const size = SIDE * SIDE
-    const candidates = shuffle(SYMBOLS)
+    const result = [];
+    const size = SIDE * SIDE;
+    const candidates = shuffle(SYMBOLS);
     while (result.length < size) {
-      const card = candidates.pop()
-      result.push(card, card)
+      const card = candidates.pop();
+      result.push(card, card);
     }
-    return shuffle(result)
+    return shuffle(result);
   }
 
-  handleCardClick(card) {
-    console.log(card, 'clicked')
-  }
+  // Arrow fx for binding
+  handleCardClick = card => {
+    console.log(card, this);
+  };
 
   render() {
-    const won = new Date().getSeconds() % 2 === 0
+    const won = new Date().getSeconds() % 2 === 0;
     return (
       <div className="memory">
         <GuessCount guesses={0} />
         {this.cards.map((card, index) => (
-          <Card card={card} feedback="visible" onClick={this.handleCardClick} key={index} />
+          <Card
+            card={card}
+            feedback="visible"
+            onClick={this.handleCardClick}
+            key={index}
+          />
         ))}
         {won && <HallOfFame entries={FAKE_HOF} />}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
